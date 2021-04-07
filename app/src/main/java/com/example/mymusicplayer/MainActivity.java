88933d5a045e13db11ceed319cc8d5383d2b0bd0
Abdullah272056 <abdullah272056@gmail.com>
2021-04-07 23:52:05 +0600

    design change

--- app/src/main/java/com/example/mymusicplayer/MainActivity.java
+++ app/src/main/java/com/example/mymusicplayer/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.mymusicplayer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-
+    String[]items;
+    ListView listView;
+    public static final int PERMISSION_READ = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
