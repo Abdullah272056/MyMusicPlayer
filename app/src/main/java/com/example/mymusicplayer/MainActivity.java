4c3384e880474764aa790999e5214e43ff6e050c
Abdullah272056 <abdullah272056@gmail.com>
2021-04-08 08:08:29 +0600

    playButton function

--- app/src/main/java/com/example/mymusicplayer/MainActivity.java
+++ app/src/main/java/com/example/mymusicplayer/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.cardview.widget.CardView;
 
 import android.Manifest;
 
+import android.content.Intent;
 import android.media.MediaMetadataRetriever;
 import android.media.MediaPlayer;
 import android.net.Uri;
@@ -77,11 +78,47 @@ public class MainActivity extends AppCompatActivity {
       //  visualizer=findViewById(R.id.blast);
 
 
-
-
         arrayList1=new ArrayList<>();
         runTimePermission();
 
+        if (mediaPlayer!=null){
+            mediaPlayer.stop();
+            mediaPlayer.release();
+        }
+
+        // data receive
+        Intent intent=getIntent();
+        Bundle bundle=intent.getExtras();
+        mySongs=(ArrayList) bundle.getParcelableArrayList("songs");
+
+        String songName=intent.getStringExtra("songName");
+        position=bundle.getInt("pos",0);
+        txtSName.setSelected(true);
+
+        Uri uri=Uri.parse(mySongs.get(position).toString());
+        sName=mySongs.get(position).getName();
+        txtSName.setText(sName);
+
+        mediaPlayer=MediaPlayer.create(getApplicationContext(),uri);
+        mediaPlayer.start();
+
+
+
+
+
+
+        btnPlay.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if (mediaPlayer.isPlaying()){
+                    btnPlay.setBackgroundResource(R.drawable.play_ic);
+                    mediaPlayer.pause();
+                }else {
+                    btnPlay.setBackgroundResource(R.drawable.pause_ic);
+                    mediaPlayer.start();
+                }
+            }
+        });
 
 
     }
@@ -102,8 +139,6 @@ public class MainActivity extends AppCompatActivity {
             }
         }).check();
     }
-
-
     ArrayList<File> findSong(File file){
         ArrayList<File>  arrayList=new ArrayList<>();
         File[] files=file.listFiles();
@@ -120,7 +155,6 @@ public class MainActivity extends AppCompatActivity {
         arrayList1.addAll(arrayList);
         return arrayList;
     }
-
     void displaySong(){
         final ArrayList<File> mySongs=findSong(Environment.getExternalStorageDirectory());
         items=new String[mySongs.size()];
@@ -193,8 +227,6 @@ public class MainActivity extends AppCompatActivity {
             return myView ;
         }
     }
-
-
     //time conversion int value to time format
     public String createTime(int duration) {
         String time="";
@@ -210,4 +242,8 @@ public class MainActivity extends AppCompatActivity {
         }
         return time;
     }
+
+
+
+
 }
