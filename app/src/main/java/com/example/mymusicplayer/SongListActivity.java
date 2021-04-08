f342fd67f8dba00432343369094b91fdf69d3277
Abdullah272056 <abdullah272056@gmail.com>
2021-04-08 07:40:08 +0600

    listView finding

--- app/src/main/java/com/example/mymusicplayer/SongListActivity.java
+++ app/src/main/java/com/example/mymusicplayer/SongListActivity.java
@@ -3,12 +3,17 @@ package com.example.mymusicplayer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class SongListActivity extends AppCompatActivity {
+    String[]items;
+    ListView listView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_song_list);
+        listView=findViewById(R.id.listView);
     }
 }
