008c94252d0ec4ed7cc9aea6aaa7f65b6d682514
Abdullah272056 <abdullah272056@gmail.com>
2021-04-08 07:42:47 +0600

    runTimePermission and display song method copy from mainActivity

--- app/src/main/java/com/example/mymusicplayer/SongListActivity.java
+++ app/src/main/java/com/example/mymusicplayer/SongListActivity.java
@@ -2,18 +2,62 @@ package com.example.mymusicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.os.Bundle;
 import android.widget.ListView;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SongListActivity extends AppCompatActivity {
     String[]items;
     ListView listView;
-
+    ArrayList<File> arrayList1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_song_list);
         listView=findViewById(R.id.listView);
+        arrayList1=new ArrayList<>();
+    }
+
+    public void runTimePermission(){
+        Dexter.withContext(SongListActivity.this).withPermissions(Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.RECORD_AUDIO)
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport multiplePermissionsReport) {
+                        //displaySong();
+                    }
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> list, PermissionToken permissionToken) {
+                        permissionToken.continuePermissionRequest();
+                    }
+                }).check();
+    }
+
+
+    ArrayList<File> findSong(File file){
+        ArrayList<File>  arrayList=new ArrayList<>();
+        File[] files=file.listFiles();
+        for (File singleFile:files){
+            if (singleFile.isDirectory() && !singleFile.isHidden()){
+                arrayList.addAll(findSong(singleFile));
+            }else {
+                if (singleFile.getName().endsWith(".mp3")  || singleFile.getName().endsWith(".wav")){
+                    arrayList.add(singleFile);
+                }
+            }
+        }
+
+        arrayList1.addAll(arrayList);
+        return arrayList;
     }
 }
