2075783a2c30b7dbcee7590e2e777f624334ed2d
Abdullah272056 <abdullah272056@gmail.com>
2021-04-11 01:53:04 +0600

    get color

--- app/src/main/java/com/example/mymusicplayer/SongListActivity.java
+++ app/src/main/java/com/example/mymusicplayer/SongListActivity.java
@@ -64,7 +64,7 @@ public class SongListActivity extends AppCompatActivity {
     Toolbar toolbar;
     ThemeDataBaseHelper themeDataBaseHelper;
     LinearLayout linearLayout;
-    Button saveButton ,cancelButton;
+
     int colorStatus;
     DrawerLayout drawerLayout;
     NavigationView navigationView;
@@ -76,6 +76,8 @@ public class SongListActivity extends AppCompatActivity {
 
     CardView color1,color2,color3,color4,color5,color6,color7,color8,
              color9,color10,color11,color12,color13,color14,color15,color16;
+    Button saveButton ,cancelButton;
+    View selectColor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -330,10 +332,15 @@ public class SongListActivity extends AppCompatActivity {
         color14=view.findViewById(R.id.color14);
         color15=view.findViewById(R.id.color15);
         color16=view.findViewById(R.id.color16);
+        selectColor=view.findViewById(R.id.selectColor);
+        cancelButton=view.findViewById(R.id.cancelButton);
+        saveButton=view.findViewById(R.id.okButton);
+
         color1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 colorStatus=1;
+                selectColor.setBackgroundColor(getResources().getColor(R.color.colorAccent));
             }
         });
         color2.setOnClickListener(new View.OnClickListener() {
